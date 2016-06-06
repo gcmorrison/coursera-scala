a91306c1faf9b8d70df26a79162155903a8fddeb
Campbell <cammorr@gmail.com>
2016-06-06 21:53:57 +0200

    Did exercise 1 and 2 of assignment 1

--- 01progfun/assignment1/recfun/src/main/scala/recfun/Main.scala
+++ 01progfun/assignment1/recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,28 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c <= 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def accumulate(acc: Int, chars: List[Char]): Boolean = {
+      if (chars.isEmpty) acc == 0
+      else if (chars.head == ')' && acc == 0) false
+      else if (chars.head == '(') accumulate(acc + 1, chars.tail)
+      else if (chars.head == ')') accumulate(acc - 1, chars.tail)
+      else accumulate(acc, chars.tail)
+    }
+    accumulate(0, chars)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
